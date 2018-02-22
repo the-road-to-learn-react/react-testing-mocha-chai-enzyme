e97d640f958d3673885fd3b43deb65692764dc64
Robin Wieruch <wrobin@gmx.net>
2018-02-22 18:48:06 +0100

    setup enzyme & add enzyme tests

--- src/App.spec.js
+++ src/App.spec.js
@@ -18,67 +18,67 @@ describe('Business Logic', () => {
   });
 });
 
-// describe('App', () => {
-//   let result = [3, 5, 9];
-//   let promise = new Promise((resolve) => resolve(result));
+describe('App', () => {
+  let result = [3, 5, 9];
+  let promise = new Promise((resolve) => resolve(result));
 
-//   before(() => {
-//     sinon.stub(axios, 'get').withArgs('http://mydomain/counter').returns(promise);
-//   });
+  // before(() => {
+  //   sinon.stub(axios, 'get').withArgs('http://mydomain/counter').returns(promise);
+  // });
 
-//   after(() => {
-//     axios.get.restore();
-//   });
+  // after(() => {
+  //   axios.get.restore();
+  // });
 
-//   it('renders the Counter wrapper', () => {
-//     const wrapper = shallow(<App />);
-//     expect(wrapper.find(Counter)).to.have.length(1);
-//   });
+  it('renders the Counter wrapper', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Counter)).to.have.length(1);
+  });
 
-//   it('passes all props to its child wrapper', () => {
-//     const wrapper = shallow(<App />);
-//     let counterWrapper = wrapper.find(Counter);
+  it('passes all props to its child wrapper', () => {
+    const wrapper = shallow(<App />);
+    let counterWrapper = wrapper.find(Counter);
 
-//     expect(counterWrapper.props().counter).to.equal(0);
+    expect(counterWrapper.props().counter).to.equal(0);
 
-//     wrapper.setState({ counter: -1 });
+    wrapper.setState({ counter: -1 });
 
-//     counterWrapper = wrapper.find(Counter);
-//     expect(counterWrapper.props().counter).to.equal(-1);
-//   });
+    counterWrapper = wrapper.find(Counter);
+    expect(counterWrapper.props().counter).to.equal(-1);
+  });
 
-//   it('increments the counter', () => {
-//     const wrapper = shallow(<App />);
+  it('increments the counter', () => {
+    const wrapper = shallow(<App />);
 
-//     wrapper.setState({ counter: 0 });
-//     wrapper.find('button').at(0).simulate('click');
+    wrapper.setState({ counter: 0 });
+    wrapper.find('button').at(0).simulate('click');
 
-//     expect(wrapper.state().counter).to.equal(1);
-//   });
+    expect(wrapper.state().counter).to.equal(1);
+  });
 
-//   it('decrements the counter', () => {
-//     const wrapper = shallow(<App />);
+  it('decrements the counter', () => {
+    const wrapper = shallow(<App />);
 
-//     wrapper.setState({ counter: 0 });
-//     wrapper.find('button').at(1).simulate('click');
+    wrapper.setState({ counter: 0 });
+    wrapper.find('button').at(1).simulate('click');
 
-//     expect(wrapper.state().counter).to.equal(-1);
-//   });
+    expect(wrapper.state().counter).to.equal(-1);
+  });
 
-//   it('calls render', () => {
-//     sinon.spy(App.prototype, 'render');
+  // it('calls render', () => {
+  //   sinon.spy(App.prototype, 'render');
 
-//     const wrapper = mount(<App />);
-//     expect(App.prototype.render.calledOnce).to.equal(true);
-//   });
+  //   const wrapper = mount(<App />);
+  //   expect(App.prototype.render.calledOnce).to.equal(true);
+  // });
 
-//   it('fetches an async counter', () => {
-//     const wrapper = shallow(<App />);
+  // it('fetches an async counter', () => {
+  //   const wrapper = shallow(<App />);
 
-//     expect(wrapper.state().asyncCounter).to.equal(null);
+  //   expect(wrapper.state().asyncCounter).to.equal(null);
 
-//     return promise.then(() => {
-//       expect(wrapper.state().asyncCounter).to.equal(result);
-//     });
-//   });
-// });
+  //   return promise.then(() => {
+  //     expect(wrapper.state().asyncCounter).to.equal(result);
+  //   });
+  // });
+});
--- test/helpers.js
+++ test/helpers.js
@@ -1,14 +1,14 @@
 // import sinon from 'sinon';
 import { expect } from 'chai';
-// import { mount, render, shallow, configure} from 'enzyme';
-// import Adapter from 'enzyme-adapter-react-16';
+import { mount, render, shallow, configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-// configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() });
 
 global.expect = expect;
 
 // global.sinon = sinon;
 
-// global.mount = mount;
-// global.render = render;
-// global.shallow = shallow;
+global.mount = mount;
+global.render = render;
+global.shallow = shallow;
